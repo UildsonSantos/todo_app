f2338dcdce7faf0eb1034c7e6ab412e751ad03e7
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-05-06 00:12:17 -0300

    Getting the correct initial state of ActiveTodoCount and FilteredTodos

--- lib/providers/active_todo_count.dart
+++ lib/providers/active_todo_count.dart
@@ -1,5 +1,7 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
+
 import 'package:todo_provider/models/todo_model.dart';
 import 'package:todo_provider/providers/providers.dart';
 
@@ -30,7 +32,18 @@ class ActiveTodoCountState extends Equatable {
 }
 
 class ActiveTodoCount with ChangeNotifier {
-  ActiveTodoCountState _state = ActiveTodoCountState.initial();
+  // ActiveTodoCountState _state = ActiveTodoCountState.initial();
+
+  late ActiveTodoCountState _state;
+  final int initialActiveTodoCount;
+
+  ActiveTodoCount({
+    required this.initialActiveTodoCount,
+  }){
+    _state = ActiveTodoCountState(activeTodoCount: initialActiveTodoCount);
+
+  }
+
   ActiveTodoCountState get state => _state;
 
   void update(TodoList todoList) {
